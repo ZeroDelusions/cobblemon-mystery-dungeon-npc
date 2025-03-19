a32d1165b25a532eb45c94f3f0be7801a209df04
ZeroDelusions <dmytro.koso@gmail.com>
2025-03-19 07:23:51 +0100

    Added necessary registration in mod init

--- src/main/kotlin/com/zero_delusions/cobblemon_mystery_dungeon_npc/CobblemonMysteryDungeonNPC.kt
+++ src/main/kotlin/com/zero_delusions/cobblemon_mystery_dungeon_npc/CobblemonMysteryDungeonNPC.kt
@@ -1,6 +1,14 @@
 package com.zero_delusions.cobblemon_mystery_dungeon_npc
 
+import com.cobblemon.mod.common.api.events.CobblemonEvents
+import com.cobblemon.mod.common.api.pokemon.PokemonSpecies
+import com.cobblemon.mod.common.api.properties.CustomPokemonProperty
+import com.zero_delusions.cobblemon_mystery_dungeon_npc.core.command.PokeNpcCommands
+import com.zero_delusions.cobblemon_mystery_dungeon_npc.core.config.ServerConfig
+import com.zero_delusions.cobblemon_mystery_dungeon_npc.entity.pokenpc.AllowedPokeNpc
+import com.zero_delusions.cobblemon_mystery_dungeon_npc.entity.pokenpc.properties.PokeNpcProperty
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import org.slf4j.LoggerFactory
 
 object CobblemonMysteryDungeonNPC : ModInitializer {
@@ -12,5 +20,12 @@ object CobblemonMysteryDungeonNPC : ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		logger.info("Hello Fabric world!")
+
+		ServerConfig.properLoad()
+
+		PokemonSpecies.observable.subscribe {
+			AllowedPokeNpc.init();
+		}
+		CommandRegistrationCallback.EVENT.register(PokeNpcCommands::register)
 	}
 }
